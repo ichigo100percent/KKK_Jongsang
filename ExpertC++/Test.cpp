77206fafe5205f5da4321f4e0e4526647c5057ad
종상 이 <rhtjddla2@gmail.com>
2023-09-15 19:36:44 +0900

    asdasdasd

--- ExpertC++/Test.cpp
+++ ExpertC++/Test.cpp
@@ -1,15 +1,11 @@
 #include "Test.h"
 
-struct Vec4
-{
-	float v[4];
-};
+
 
 bool Test::Init()
 {
 
-	vector<int> a(50, 0);
-
+	
 
 	return true;
 }
---
+++ KKK_Jongsang/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+using namespace std;
+
+
+
+int main()
+{
+
+}
