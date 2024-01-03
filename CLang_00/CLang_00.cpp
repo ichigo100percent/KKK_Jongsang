aef554ffc615b2dfb06ef94e4617d1186c88fd7e
종상 이 <rhtjddla2@gmail.com>
2024-01-03 17:54:40 +0900

    sdgdsg

--- CLang_00/CLang_00.cpp
+++ CLang_00/CLang_00.cpp
@@ -8,38 +8,5 @@
 
 int main()
 {
-	printf("\n==========포인터 배열========\n");
 
-	system("cls");
-
-	char* pArray[6];
-
-	for (int i = 0; i < 6; i++)
-	{
-		pArray[i] = (char*)malloc(sizeof(char) * 4);
-
-	}
-
-	for (int i = 0; i < 6; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			pArray[i][j] = 65 + i * 3 + j;
-		}
-		//문자열 마지막은 null로 한다.
-		
-		pArray[i][3] = 0;
-	}
-
-	for (int i = 0; i < 6; i++)
-	{
-		printf("\n");
-		printf("[%d]=%s", i, pArray[i]);
-	}
-	for (int i = 0; i < 6; i++)
-	{
-		free(pArray[i]);
-	}
-
-	_getch();
 }
